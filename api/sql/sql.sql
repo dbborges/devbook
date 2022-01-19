2ffbebfb2a32afd732bb70828fcd475636fdc0de
Daniel Borges <dbborges.rj@gmail.com>
2022-01-19 10:24:38 -0300

    Ajustando script sql da tabela usuarios Criando implementação para a função CriarUsuario do controller Criando repositório de usuários

--- api/sql/sql.sql
+++ api/sql/sql.sql
@@ -3,10 +3,12 @@ USER devbook;
 
 DROP TABLE IF EXISTS usuarios;
 
-CREATE TABLE usuario{
-    id int auto_increment primary key,
-    nome varchar(50) not null,
-    nick varchar(50) not null unique,
-    senha varchar(20) not null unique,
+CREATE TABLE usuarios
+(
+    id       int auto_increment primary key,
+    nome     varchar(50) not null,
+    nick     varchar(50) not null unique,
+    email    varchar(50) not null unique,
+    senha    varchar(20) not null unique,
     criadoEm timestamp default current_timestamp()
-} ENGINE=INNODB;
+) ENGINE=INNODB;
