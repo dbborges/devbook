1812a7a7943bbae7bd58a6d776dcb5ce8acb4314
Daniel Borges <dbborges.rj@gmail.com>
2022-01-23 10:02:44 -0300

    Criando estrutura para carregamento e execução dos templates

---
+++ webapp/src/router/rotas/rotas.go
@@ -0,0 +1,24 @@
+package rotas
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+// Rota representa todas as rotas da aplicação web
+type Rota struct {
+	URI                string
+	Metodo             string
+	Funcao             func(w http.ResponseWriter, r *http.Request)
+	RequerAutenticacao bool
+}
+
+// Configurar coloca todas as rotas dentro do router
+func Configurar(router *mux.Router) *mux.Router {
+	rotas := rotasLogin
+
+	for _, rota := range rotas {
+		router.HandleFunc(rota.URI, rota.Funcao).Methods(rota.Metodo)
+	}
+	return router
+}
