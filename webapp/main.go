d26d2a664ea73ebaa8368ccb0554f1e8c33c7122
Daniel Borges <dbborges.rj@gmail.com>
2022-01-27 13:18:22 -0300

    Implementando cookies no login

--- webapp/main.go
+++ webapp/main.go
@@ -4,14 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"webapp/src/config"
+	"webapp/src/cookies"
 	"webapp/src/router"
 	"webapp/src/utils"
 )
 
 func main() {
+	config.Carregar()
+	cookies.Configurar()
 	utils.CarregarTemplates()
 	r := router.Gerar()
 
-	fmt.Println("Escutando na porta 3000!")
-	log.Fatal(http.ListenAndServe(":3000", r))
+	fmt.Println(config.Porta)
+
+	fmt.Printf("Escutando na porta %d\n", config.Porta)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 }
